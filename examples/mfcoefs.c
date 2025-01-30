aa72c483c4735a027469303481d0f27dd4a13e4b
Pascal <molin.maths@gmail.com>
2025-01-30 13:51:54 +0100

    try better euler products: no gain

--- examples/mfcoefs.c
+++ examples/mfcoefs.c
@@ -15,7 +15,7 @@
 /* fast computation of modular forms coefficients.
    Use precomputed representation as Eisenstein series */
 
-/* modular forms expressions using Eisenstein series */
+/* modular form expression via Eisenstein series */
 
 struct mf_eis_desc {
     const slong N;
@@ -27,7 +27,7 @@ struct mf_eis_desc {
 
     const slong ord;     /* order of root of unity */
     const slong modp;    /* fft prime used for expression */
-    const slong z;       /* root of unity */
+    const slong z;       /* root of unity for character */
 
     const slong denom;   /* denominator to take into account */
     const slong * E0;    /* (1+2*nchi) constant values of E1(chi)*denom */
@@ -130,7 +130,25 @@ _nmod_poly_set_euler_factor(nn_ptr z, slong len, nn_srcptr c, slong deg, slong p
         for (m = 1, pem = pe; pem < len; m++, pem += pe)
             for(r = 1; r < p && pem < len; m++, pem += pe, r++)
                 z[pem] = nmod_mul(z[m], c[e], mod);
-    } 
+    }
+}
+
+void
+_nmod_poly_set_euler_factor_conj(nn_ptr z1, nn_ptr z2, slong len, nn_srcptr c1, nn_srcptr c2, slong deg, slong p, nmod_t mod)
+{
+    slong e, pe;
+    for (e = 1, pe = p; e <= deg; e++, pe *= p)
+    {
+        slong m, pem, r;
+        for (m = 1, pem = pe; pem < len; m++, pem += pe)
+        {
+            for(r = 1; r < p && pem < len; m++, pem += pe, r++)
+            {
+                z1[pem] = nmod_mul(z1[m], c1[e], mod);
+                z2[pem] = nmod_mul(z2[m], c2[e], mod);
+            }
+        }
+    }
 }
 
 /* euler factor of Eisenstein series, 1/((1-chi(p)*x)*(1-p*x)) */
@@ -181,6 +199,70 @@ _nmod_poly_euler_product(nn_ptr z, slong len, _nmod_euler_func_t factor, void * 
     n_primes_clear(iter);
 }
 
+slong
+_nmod_poly_set_euler_factor_small(nn_ptr z, slong len, nn_srcptr c, slong deg, slong p, slong * s, slong ns, nmod_t mod)
+{
+    slong e, pe, ns1 = ns;
+    for (e = 1, pe = p; e <= deg; e++, pe *= p)
+    {
+        slong i;
+        for (i = 0; i < ns; i++)
+        {
+            slong m = s[i], pem = pe*m;
+            if (pem > len) continue;
+            z[pem] = nmod_mul(z[m], c[e], mod);
+            s[ns1++] = pem;
+        }
+    }
+    return ns1;
+}
+
+void
+_nmod_poly_euler_product_stages(nn_ptr z, slong len, _nmod_euler_func_t factor, void * ctx, nmod_t mod)
+{
+    n_primes_t iter;
+    slong p, B, ns;
+    slong * s;
+    ulong fp[30];
+    n_primes_init(iter);
+
+    /* number of smooth indices stored */
+    B = n_cbrt(len);
+
+    s = (slong *)flint_malloc(len * sizeof(slong));
+    z[1] = 1;
+
+    for (p = n_primes_next(iter); p < len; p = n_primes_next(iter))
+    {
+        slong deg = n_flog(len, p);
+        factor(fp, deg, p, ctx, mod);
+        if (p < B && ns < len)
+            ns = _nmod_poly_set_euler_factor_small(z, len, fp, deg, p, s, ns, mod);
+        else
+            _nmod_poly_set_euler_factor(z, len, fp, deg, p, mod);
+    }
+    n_primes_clear(iter);
+    flint_free(s);
+}
+
+void
+_nmod_poly_euler_product_conj(nn_ptr z1, nn_ptr z2, slong len, _nmod_euler_func_t factor, void * ctx, void * ctx2, nmod_t mod)
+{
+    n_primes_t iter;
+    slong p;
+    ulong fp1[30], fp2[30];
+    n_primes_init(iter);
+
+    z1[1] = z2[1] = 1;
+    for (p = n_primes_next(iter); p < len; p = n_primes_next(iter))
+    {
+        slong deg = n_flog(len, p);
+        factor(fp1, deg, p, ctx, mod);
+        factor(fp2, deg, p, ctx2, mod);
+        _nmod_poly_set_euler_factor_conj(z1, z2, len, fp1, fp2, deg, p, mod);
+    }
+    n_primes_clear(iter);
+}
 /* currently only output constant coefficients mod poly */
 void
 _fmpz_modular_form_expansion(fmpz * a, slong len, const struct mf_eis_desc f)
@@ -200,7 +282,7 @@ _fmpz_modular_form_expansion(fmpz * a, slong len, const struct mf_eis_desc f)
 
     g = _nmod_vec_init(len2);
 
-    /* initialize with c[0] * E_2(1 mod N) */ 
+    /* initialize with c[0] * E_2(1 mod N) */
     {
         ulong c = nmod_set_si(f.coefs[0], mod);
         g[0] = 0;
@@ -216,20 +298,30 @@ _fmpz_modular_form_expansion(fmpz * a, slong len, const struct mf_eis_desc f)
     g12 = _nmod_vec_init(len2);
     for (k = 0; k < f.nchi; k++)
     {
-        mf_eis_ctx_t ctx;
+        mf_eis_ctx_t ctx, ctx2;
         ulong c = nmod_set_si(f.coefs[(k+1)*f.degy], mod);
 
+        TIMEIT_ONCE_START
+        flint_printf("[ char %ld ]\n",k+1);
         mf_eis_ctx_init(ctx, G, f.chi[k], f.ord, f.z, mod);
+        TIMEIT_ONCE_STOP
 
         TIMEIT_ONCE_START
-        flint_printf("[ char %ld ] euler g1...", k+1);
-        g1[0] = nmod_set_si(f.E0[2*k], mod);
-        _nmod_poly_euler_product(g1, len2, _nmod_euler_factor_E1_chi, ctx, mod);
+        flint_printf("##    euler g1 and g2...", k+1);
+        mf_eis_ctx_init(ctx2, G, f.chi[k], f.ord, nmod_inv(f.z,mod), mod);
+        _nmod_poly_euler_product_conj(g1, g2, len2, _nmod_euler_factor_E1_chi, ctx, ctx2, mod);
         flint_printf("[done]\n");
         TIMEIT_ONCE_STOP
 
         TIMEIT_ONCE_START
-        flint_printf("           euler g2...");
+        flint_printf("##    euler g1 with stages...", k+1);
+        g1[0] = nmod_set_si(f.E0[2*k], mod);
+        _nmod_poly_euler_product_stages(g1, len2, _nmod_euler_factor_E1_chi, ctx, mod);
+        flint_printf("[done]\n");
+        TIMEIT_ONCE_STOP
+
+        TIMEIT_ONCE_START
+        flint_printf("##    euler g2 direct...");
         mf_eis_ctx_dual(ctx, mod);
         g2[0] = nmod_set_si(f.E0[2*k+1], mod);
         _nmod_poly_euler_product(g2, len2, _nmod_euler_factor_E1_chi, ctx, mod);
@@ -253,7 +345,7 @@ _fmpz_modular_form_expansion(fmpz * a, slong len, const struct mf_eis_desc f)
         TIMEIT_ONCE_STOP
 #endif
         _nmod_vec_scalar_addmul_nmod(g, g12, len2, c, mod);
- 
+
     }
     _nmod_vec_clear(g12);
     _nmod_vec_clear(g1);
@@ -270,114 +362,6 @@ _fmpz_modular_form_expansion(fmpz * a, slong len, const struct mf_eis_desc f)
 
     _nmod_vec_clear(g);
     dirichlet_group_clear(G);
-}
-
-/* same with fmpz */
-
-/* assume deg s.t. p^deg < len */
-void
-_fmpz_poly_set_euler_si_mul(fmpz *z, slong len, const slong *c, slong deg, slong p)
-{
-    slong e, pe;
-    for (e = 1, pe = p; e <= deg; e++, pe *= p)
-    {
-        slong m, pem, r;
-        for (m = 1, pem = pe; pem < len; m++, pem += pe)
-            for(r = 1; r < p && pem < len; m++, pem += pe, r++)
-                fmpz_mul_si(z + pem, z+m, c[e]);
-    } 
-}
-
-/* euler factor of Eisenstein series, 1/((1-chi(p)*x)*(1-p*x)) */
-typedef void _fmpz_euler_func_t(fmpz *fp, slong deg, slong p, void * ctx);
-
-/* E_2( Mod(1,N) ) as prod_p ((1-p^(-s))*(1-p^(1-s)))^(-1). Assume N prime. */
-void
-_fmpz_euler_factor_E2_1N(fmpz *fp, slong deg, slong p, void * ctx)
-{
-    long N = (long)ctx;
-    if (p == N)
-    {
-        fmpz Q[2] = { 1, -1 };
-        _fmpz_poly_inv_series(fp, Q, 2, deg+1);
-    }
-    else
-    {
-        /* (1-p*x)*(1-x) = p*x^2 + (-p - 1)*x + 1 */
-        fmpz Q[3] = { 1, -p-1, p };
-        _fmpz_poly_inv_series(fp, Q, 3, deg+1);
-    }
-}
-
-/* E_1( (-11/.) ) as prod_p ((1-(-11/p)p^(-s))*(1-p^(-s)))^(-1) */
-/* warning: ignore a0 = 1/2 */
-void
-_fmpz_euler_factor_E1_11(fmpz *fp, slong deg, slong p, void * ctx)
-{
-    slong chip = n_jacobi(-11, p);
-    fmpz Q[3] = { 1, -chip-1, chip }; /* (1-chip*x)*(1-x) */
-    _fmpz_poly_inv_series(fp, Q, 3, deg+1);
-}
-
-void
-_fmpz_poly_euler_product(fmpz *z, slong len, _fmpz_euler_func_t factor, void * ctx)
-{
-
-    n_primes_t iter;
-    slong p;
-    n_primes_init(iter);
-    z[1] = 1;
-    for (p = n_primes_next(iter); p < len; p = n_primes_next(iter))
-    {
-        slong deg = n_flog(len, p);
-        fmpz * fp = _fmpz_vec_init(deg);
-        factor(fp, deg, p, ctx);
-        _fmpz_poly_set_euler_si_mul(z, len, fp, deg, p);
-        _fmpz_vec_clear(fp, deg);
-    }
-    n_primes_clear(iter);
-}
-
-void
-_fmpz_modular_form_f11_expansion(fmpz * a, slong len)
-{
-    /* euler product */
-    fmpz *g1, *g2;
-    g1 = _fmpz_vec_init(len);
-    g2 = _fmpz_vec_init(len);
-
-    TIMEIT_ONCE_START
-    flint_printf("compute a = -3 * (1*id) as Euler product...");
-    fmpz_set_si(a + 0, 0);
-    _fmpz_poly_euler_product(a, len, _fmpz_euler_factor_E2_1N, (void *)11);
-    _fmpz_vec_scalar_mul_si(a, a, len, -3);
-    flint_printf("[done]\n");
-    TIMEIT_ONCE_STOP
-
-    TIMEIT_ONCE_START
-    flint_printf("compute g1 = 1*chi as Euler product...");
-    fmpz_set_si(g1 + 0, 0);
-    _fmpz_poly_euler_product(g1, len, _fmpz_euler_factor_E1_11, NULL);
-    flint_printf("[done]\n");
-    TIMEIT_ONCE_STOP
-
-    TIMEIT_ONCE_START
-    flint_printf("compute g2 = g1^2 + 2g1*a0 = g1^2+g1...");
-    _fmpz_poly_mullow(g2, g1, len, g1, len, len);
-    _fmpz_poly_add(g2, g2, len, g1, len); /* + 2*f*a0 = f */
-    flint_printf("[done]\n");
-    TIMEIT_ONCE_STOP
-
-    TIMEIT_ONCE_START
-    flint_printf("compute ( a += 5*g2 ) / 2...");
-    _fmpz_vec_scalar_addmul_si(a, g2, len, 5);
-    _fmpz_vec_scalar_tdiv_q_2exp(a, a, len, 1);
-    a[0] = 0;
-    flint_printf("[done]\n");
-    TIMEIT_ONCE_STOP
-
-    _fmpz_vec_clear(g1, len);
-    _fmpz_vec_clear(g2, len);
 }
 
 int main(int argc, char* argv[])
@@ -401,7 +385,6 @@ int main(int argc, char* argv[])
     a = _fmpz_vec_init(len);
 
     if (N == 11)
-        //_fmpz_modular_form_f11_expansion(a, len);
         _fmpz_modular_form_expansion(a, len, f11);
     else if (N == 31)
         _fmpz_modular_form_expansion(a, len, f31);
